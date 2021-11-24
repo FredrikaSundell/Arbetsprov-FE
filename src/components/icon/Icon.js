532d8840c1667000f35b0ef81abebde56849e8ea
fredrikasundell <fredrikasundell@gmail.com>
2021-11-24 15:00:52 +0100

    icons

--- src/components/icon/Icon.js
+++ src/components/icon/Icon.js
@@ -1,4 +1,8 @@
 import Sun from './../icons/sun'
+import Cloudy from './../icons/cloudy'
+import Rain from './../icons/rain'
+import Snow from './../icons/snow'
+import Fallback from './../icons/fallback'
 import './icon.scss'
 
 function Icon() {
